37fdb5253c1937277ebb9652507a5c3138effbb3
hiwon-lee <84234411+hiwon-lee@users.noreply.github.com>
2025-10-16 23:54:00 +0900

    :recycle: global error code

--- src/main/java/com/example/tikitaka/global/config/auth/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/example/tikitaka/global/config/auth/CustomAuthenticationEntryPoint.java
@@ -1,5 +1,8 @@
 package com.example.tikitaka.global.config.auth;
 
+import com.example.tikitaka.global.exception.ErrorReason;
+import com.example.tikitaka.global.exception.GlobalErrorCode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.AuthenticationException;
@@ -10,15 +13,26 @@ import java.io.IOException;
 
 @Component
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
     public void commence(
             HttpServletRequest request,
             HttpServletResponse response,
             AuthenticationException authException
     ) throws IOException {
-        response.setStatus(HttpServletResponse.SC_FORBIDDEN); // 403
+
+        // 전역 에러 코드
+        GlobalErrorCode errorCode = GlobalErrorCode.ACCESS_DENIED;
+        ErrorReason reason = errorCode.getErrorReason();
+
+        response.setStatus(errorCode.getStatus().value());
         response.setContentType("application/json;charset=UTF-8");
-        response.getWriter().write("{\"error\": \"Access denied. Authentication required.\"}");
+
+        // JSON으로 변환해서 응답
+        String responseBody = objectMapper.writeValueAsString(reason);
+        response.getWriter().write(responseBody);
     }
 }
 
