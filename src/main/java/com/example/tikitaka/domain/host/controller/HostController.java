3a929024dc9951afe25569e6753837a4b97e3f26
hiwon-lee <84234411+hiwon-lee@users.noreply.github.com>
2025-10-27 16:38:37 +0900

    :seedling: 후기 작성 여부 필드 추가

---
+++ src/main/java/com/example/tikitaka/domain/host/controller/HostController.java
@@ -0,0 +1,60 @@
+package com.example.tikitaka.domain.host.controller;
+
+import com.example.tikitaka.domain.exhibition.dto.ExhibitionCreate;
+import com.example.tikitaka.domain.exhibition.dto.request.ExhibitionPostRequest;
+import com.example.tikitaka.domain.exhibition.dto.response.ExhibitionDetailResponse;
+import com.example.tikitaka.infra.s3.S3Url;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/host")
+public class HostController {
+
+    // host
+    @PostMapping
+    public void exhibitionAdd(
+            @RequestBody ExhibitionPostRequest request
+    ) {
+        exhibitionService.addExhibition(request);
+    }
+
+    @GetMapping("/{exhibitionId}")
+    public ExhibitionDetailResponse exhibitionDetail(
+            // 로그인 기능 추가 후 유저 정보 받기
+            @PathVariable
+            Long exhibitionId
+    ){
+        return exhibitionService.findExhibition(exhibitionId);
+    }
+
+    @DeleteMapping("/{exhibitionId}")
+    public void exhibitionDelete(
+            // 로그인 기능 추가 후 유저 정보 받기
+            @PathVariable Long exhibitionId
+    ) {
+        exhibitionService.deleteExhibition(exhibitionId);
+    }
+
+    @PatchMapping("/{exhibitionId}")
+    public void exhibitionUpdate(
+            // 로그인 기능 추가 후 유저 정보 받기
+            @PathVariable Long exhibitionId,
+            @RequestBody ExhibitionCreate request
+    ) {
+        exhibitionService.updateExhibition(exhibitionId, request);
+    }
+
+    @GetMapping("/posters")
+    public S3Url getPosterUploadUrl() {
+        return exhibitionService.getImageUploadUrl();
+    }
+
+    @GetMapping("/images")
+    public S3Url getImagesUploadUrl() {
+        return exhibitionImageService.getImageUploadUrl();
+    }
+
+
+}
