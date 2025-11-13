ec86b74be35502040287c04e945363cc829f8178
jiminnimij <124450012+jiminnimij@users.noreply.github.com>
2025-11-13 19:51:23 +0900

    :ambulance: cors security에 추가

--- src/main/java/com/example/tikitaka/global/config/SecurityConfig.java
+++ src/main/java/com/example/tikitaka/global/config/SecurityConfig.java
@@ -64,6 +64,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((authorize) -> authorize
